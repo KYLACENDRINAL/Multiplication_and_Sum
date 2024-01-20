56845b960629674a3737fc7d5a5493131de59890
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-01-20 17:40:02 +0800

    pseuducode

---
+++ Multiplication_and_Sum.py
@@ -0,0 +1,9 @@
+# Calculate the multiplication and sum of two numbers
+
+# pseuducode
+# Define the function of multiplication and sum of two numbers
+# Multiply the two numbers
+# Check if the product is less than 1000
+# Calculate the sum of two numbers if the product is greater than 1000
+# Print the result of first condition
+# Print the result of second condition
